734204c40bbe8e39fec775a2cfa1b442bf19cc65
Rohitadesh <adeshnagalikar2000@gmail.com>
2023-08-05 18:50:54 +0530

    newly commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ function App() {
   const [city,setcity]=useState("")
   const [weather,setweather]=useState({})
    const weatherinfo= async (cityname)=>{
+    if(cityname===""){
+      alert("enter cityname")
+    }
+    else{
       const apikey="70592b526d9742b8b1bb7e8932ee97cd"
       const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=metric&appid=${apikey}`)
       const result= await response.json();
       setweather(result)
+
+    }
     }
   return (
     <div className=" main-box bg-[url(../public/img/w.jpg)] h-screen w-screen  flex flex-col flex-wrap justify-center items-center bg-no-repeat bg-cover bg-center bg-fixed ">      
@@ -16,7 +22,7 @@ function App() {
         <h1 className='text-3xl font-serif italic font-semibold'>Weather App</h1>
       </div>
       
-      <div className="boxes shadow-2xl weather-box w-1/3 h-3/6 border-2  rounded-lg flex flex-wrap flex-col justify-start items-center gap-16">
+      <div className="boxes shadow-2xl weather-box w-1/3 h-3/6 border-2 rounded-lg flex flex-wrap flex-col justify-start items-center gap-16">
        
         <div className='h-[15%] w-full flex flex-wrap flex-row justify-center items-center gap-2'>
 
